c7b4bde6cdac1817ac3f1f8b8c20c56d8305e4fc
T1mp4ve1 <pavelwire@outlook.it>
2025-08-25 21:25:55 +0200

    timer 2

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,23 @@
 let seconds = 60;
-const display = document.getElementsByClassName("number")[0];
+const count = document.getElementsByClassName("number")[0];
+const progress = document.getElementsByClassName("progress")[0];
+
+const radius = 45;
+const circumference = 2 * Math.PI * radius;
+
+function setProgress(percent) {
+  const offset = circumference - (percent / 100) * circumference;
+  progress.style.strokeDashoffset = offset;
+}
 
 const timer = setInterval(() => {
-  const remaining = seconds --;
-  display.innerText = remaining;
+  const remaining = seconds--;
+  count.innerText = remaining;
+
+  const percent = (seconds / 60) * 100;
+  setProgress(percent);
+
   if (remaining <= 0) {
-    clearInterval(timer)
+    clearInterval(timer);
   }
 }, 1000);
-timer()
