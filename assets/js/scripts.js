67354044c2c311e9425f7c6183d4026452384f7d
MREBECCA98 <reb.matarozzo@gmail.com>
2025-08-27 16:07:04 +0200

    question script

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -32,18 +32,13 @@ const questions = [
     difficulty: "easy",
     question: "What does CPU stand for?",
     correct_answer: "Central Processing Unit",
-    incorrect_answers: [
-      "Central Process Unit",
-      "Computer Personal Unit",
-      "Central Processor Unit",
-    ],
+    incorrect_answers: ["Central Process Unit", "Computer Personal Unit", "Central Processor Unit"],
   },
   {
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question:
-      "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
+    question: "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
     correct_answer: "Final",
     incorrect_answers: ["Static", "Private", "Public"],
   },
@@ -59,8 +54,7 @@ const questions = [
     category: "Science: Computers",
     type: "boolean",
     difficulty: "easy",
-    question:
-      "Pointers were not used in the original C programming language; they were added later on in C++.",
+    question: "Pointers were not used in the original C programming language; they were added later on in C++.",
     correct_answer: "False",
     incorrect_answers: ["True"],
   },
@@ -68,8 +62,7 @@ const questions = [
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question:
-      "What is the most preferred image format used for logos in the Wikimedia database?",
+    question: "What is the most preferred image format used for logos in the Wikimedia database?",
     correct_answer: ".svg",
     incorrect_answers: [".png", ".jpeg", ".gif"],
   },
@@ -79,18 +72,13 @@ const questions = [
     difficulty: "easy",
     question: "In web design, what does CSS stand for?",
     correct_answer: "Cascading Style Sheet",
-    incorrect_answers: [
-      "Counter Strike: Source",
-      "Corrective Style Sheet",
-      "Computer Style Sheet",
-    ],
+    incorrect_answers: ["Counter Strike: Source", "Corrective Style Sheet", "Computer Style Sheet"],
   },
   {
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question:
-      "What is the code name for the mobile operating system Android 7.0?",
+    question: "What is the code name for the mobile operating system Android 7.0?",
     correct_answer: "Nougat",
     incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
   },
@@ -114,8 +102,7 @@ const questions = [
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question:
-      "Which programming language shares its name with an island in Indonesia?",
+    question: "Which programming language shares its name with an island in Indonesia?",
     correct_answer: "Java",
     incorrect_answers: ["Python", "C", "Jakarta"],
   },
@@ -123,6 +110,7 @@ const questions = [
 
 const up = document.getElementById("up");
 const down = document.getElementById("down");
+const proceedConteiner = document.querySelector(".proceed");
 
 const question = document.querySelector("#questions h3");
 const button_1 = document.createElement("div");
@@ -140,43 +128,58 @@ const randomQuestions = (array) => {
   return array[num];
 };
 
-const randomAnswer = (question) => {
-  const num = Math.floor(Math.random() * 4);
+const allAnswer = (question) => {
   let allAnswer = [];
   allAnswer.push(question.correct_answer);
-  console.log(question)
+  console.log(question);
   for (let i = 0; i < question.incorrect_answers.length; i++) {
     allAnswer.push(question.incorrect_answers[i]);
   }
   return allAnswer;
 };
 
-console.log(randomAnswer(randomQuestions(questions)));
+const randomAnswer = (answer) => {
+  answer.sort(() => Math.random() - 0.5);
+  return answer;
+};
+const button = document.createElement("div");
+button.classList.add("button");
+const getQuestion = () => {
+  const currentQuestion = randomQuestions(questions);
+  const currentAnswer = randomAnswer(allAnswer(currentQuestion));
 
+  question.innerText = currentQuestion.question;
+  for (let i = 0; i < currentAnswer.length; i++) {
+    if (i < 2) {
+      button.innerText = currentAnswer[i];
+      up.appendChild(button);
+    } else {
+      button.innerText = currentAnswer[i];
+      down.appendChild(button);
+    }
+  }
+};
+
+button.addEventListener("click", () => {
+  button.classList.remove("button");
+  button.classList.add("button_choice");
+});
+
+console.log(randomAnswer(allAnswer(randomQuestions(questions))));
+console.log(proceedConteiner);
 const get = document.addEventListener("testPageVisible", () => {
   if (!test.hidden) {
     timerActive();
-    question.innerText = questions[0].question;
-    button_1.innerText = questions[0].correct_answer;
-    up.appendChild(button_1);
-
-    button_2.innerText = questions[0].incorrect_answers[0];
-    up.appendChild(button_2);
-
-    button_3.innerText = questions[0].incorrect_answers[1];
-    down.appendChild(button_3);
-
-    button_4.innerText = questions[0].incorrect_answers[2];
-    down.appendChild(button_4);
-
-    // let myIndex = 0;
-    // const changeQuestion = setInterval(() => {
-    //   questions.forEach((e) => {
-    //     question.innerText = e.question;
-    //   });
-    //   changeQuestion();
-    // }, 60000);
+    getQuestion();
   }
+
+  // let myIndex = 0;
+  // const changeQuestion = setInterval(() => {
+  //   questions.forEach((e) => {
+  //     question.innerText = e.question;
+  //   });
+  //   changeQuestion();
+  // }, 60000);
 });
 
 const timerActive = () => {
